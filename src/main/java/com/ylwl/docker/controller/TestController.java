d26cc4adfb97a8bdd7923d2abeafe7b5f58d14d2
huxiong <xiong.hu@minew.com>
2021-05-26 16:26:11 +0800

    feature:测试一下

--- src/main/java/com/ylwl/docker/controller/TestController.java
+++ src/main/java/com/ylwl/docker/controller/TestController.java
@@ -8,6 +8,7 @@ public class TestController {
     @GetMapping("/test")
     public String getResult(){
         int a = 1 << 2;
+        int b =a+1;
 
         return "this is a test data:"+ a;
     }
